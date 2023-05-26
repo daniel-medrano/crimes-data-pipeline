298543fca0de37632085ada78e54a5d9c609d3d3
Daniel Medrano <jdmedrano555@gmail.com>
2023-05-26 11:28:42 -0600

    Created fct_crimes model and some tests and docs.

---
+++ dbt/Crimes/models/marts/core/fct_crimes.sql
@@ -0,0 +1,44 @@
+with stg_crimes as (
+    select * from {{ ref('stg_crimes') }}
+),
+dim_modalities as (
+    select * from {{ ref('dim_modalities') }}
+),
+dim_dates as (
+    select * from {{ ref('dim_dates') }}
+),
+dim_targets as (
+    select * from {{ ref('dim_targets') }}
+),
+dim_victim_demographics as (
+    select * from {{ ref('dim_victim_demographics') }}
+),
+dim_geographics as (
+    select * from {{ ref('dim_geographics') }}
+),
+crimes as (
+    select
+        modality_id,
+        date_id,
+        target_id,
+        demographics_id,
+        geographics_id
+    from stg_crimes c
+    inner join dim_modalities m
+        on c.crime_category = m.category
+        and c.crime_modality = m.modality
+    inner join dim_dates d
+        on c.crime_date = d.date
+    inner join  dim_targets t
+        on c.target_type = t.target_type
+        and c.target_description = t.target_description
+    inner join dim_victim_demographics v
+        on c.victim_age_category = v.age_category
+        and c.victim_nationality = v.nationality
+    inner join dim_geographics g
+        on c.crime_province = g.province
+        and c.crime_canton = g.canton
+        and c.crime_district = g.district
+)
+select *
+from crimes
